59dc1e0bf4a1142b1b07acb4c424b3bac011e7c4
houwanle <wanle@qq.com>
2022-06-06 22:46:18 +0800

    添加 profiles多环境配置

--- src/main/java/com/lele/springbootstudy/controller/HelloController.java
+++ src/main/java/com/lele/springbootstudy/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.lele.springbootstudy.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
     }
+
+    @GetMapping("/queryServerPort")
+    public String queryServerPort() {
+        return serverPort;
+    }
+
 }
