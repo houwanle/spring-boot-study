ce1104b79d520764f7fc486a9ef69063d94ef274
houwanle <wanle@qq.com>
2022-06-06 22:08:55 +0800

    创建项目

---
+++ README.md
@@ -0,0 +1,8 @@
+## SpringBootStudy
+
+### 环境
+
+名称 | 版本
+---|---
+spring boot | 2.7.0
+jdk | 1.8
---
+++ src/main/java/com/lele/springbootstudy/Application.java
@@ -0,0 +1,19 @@
+package com.lele.springbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: lele
+ * @date: 2022/6/6 13:37
+ * @description:
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
