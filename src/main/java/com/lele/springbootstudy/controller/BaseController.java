1a166a0d6a01ec4dd7101ba6c0ad17663857afa5
houwanle <wanle@qq.com>
2022-06-25 19:23:43 +0800

    添加 BaseController

---
+++ src/main/java/com/lele/springbootstudy/controller/BaseController.java
@@ -0,0 +1,11 @@
+package com.lele.springbootstudy.controller;
+
+/**
+ * @author: lele
+ * @date: 2022/6/25 19:02
+ * @description:
+ */
+public class BaseController {
+
+    protected final String CHAR_SET = "application/json;charset=UTF-8";
+}
--- src/main/java/com/lele/springbootstudy/controller/HelloController.java
+++ src/main/java/com/lele/springbootstudy/controller/HelloController.java
@@ -4,6 +4,8 @@ import com.lele.springbootstudy.common.BaseErrorCode;
 import com.lele.springbootstudy.method.BaseLogMethod;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,14 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-public class HelloController {
+public class HelloController extends BaseController {
 
     private static final String TAG = "HelloController";
 
     @Value("${server.port}")
     private String serverPort;
 
-    @GetMapping("/hello")
+    @RequestMapping(value = {"/hello"}, produces = CHAR_SET, method = RequestMethod.GET)
     public String hello() {
         return "Hello World!";
     }
@@ -34,7 +36,7 @@ public class HelloController {
      * 国际化测试接口
      * @return
      */
-    @GetMapping("/message")
+    @RequestMapping(value= {"/message"}, produces = CHAR_SET, method = RequestMethod.GET)
     public String message() {
         BaseLogMethod.logInfo(TAG, "12345");
         return BaseErrorCode.getMessage("welcome");
